cacb0fdda9416843d72f612beb1ff96a213a258e
Balazs Kovacs <kovacs@adobe.com>
2016-06-08 17:05:07 +0000

    Deleted migrations and created on initial migration instead

--- migrations/0001_initial.py
+++ migrations/0001_initial.py
@@ -2,9 +2,10 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import django.utils.timezone
-import common.utils
 import common.backends
+import django.utils.timezone
+import django.db.models.deletion
+import cnntools.common_utils
 
 
 class Migration(migrations.Migration):
@@ -19,9 +20,25 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('added', models.DateTimeField(default=django.utils.timezone.now)),
                 ('description', models.TextField()),
-                ('netid', models.CharField(max_length=16, db_index=True)),
-                ('model_file', models.FileField(storage=common.backends.DeconstructibleS3BotoStorage(), max_length=255, null=True, upload_to=common.utils.FileUploadPath(b'cnntools', ext=b'prototxt'), blank=True)),
-                ('solver_file', models.FileField(storage=common.backends.DeconstructibleS3BotoStorage(), max_length=255, null=True, upload_to=common.utils.FileUploadPath(b'cnntools', ext=b'prototxt'), blank=True)),
+                ('netid', models.CharField(unique=True, max_length=64)),
+                ('model_file', models.FileField(storage=common.backends.DeconstructibleS3BotoStorage(), max_length=255, null=True, upload_to=cnntools.common_utils.FileUploadPath(b'cnntools', ext=b'prototxt'), blank=True)),
+                ('solver_file', models.FileField(storage=common.backends.DeconstructibleS3BotoStorage(), max_length=255, null=True, upload_to=cnntools.common_utils.FileUploadPath(b'cnntools', ext=b'prototxt'), blank=True)),
+                ('deploy_file', models.FileField(storage=common.backends.DeconstructibleS3BotoStorage(), max_length=255, null=True, upload_to=cnntools.common_utils.FileUploadPath(b'cnntools', ext=b'prototxt'), blank=True)),
+            ],
+            options={
+                'ordering': ['-id'],
+                'abstract': False,
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='CaffeCNNSnapshot',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('added', models.DateTimeField(default=django.utils.timezone.now)),
+                ('iteration', models.IntegerField()),
+                ('sha1', models.CharField(max_length=40)),
+                ('model_snapshot', models.FileField(storage=common.backends.DeconstructibleS3BotoStorage(), max_length=255, null=True, upload_to=cnntools.common_utils.FileUploadPathKeepName(b'cnntools/snapshots', ext=b'caffemodel'), blank=True)),
             ],
             options={
                 'ordering': ['-id'],
@@ -35,14 +52,25 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('added', models.DateTimeField(default=django.utils.timezone.now)),
                 ('final_iteration', models.IntegerField()),
+                ('max_iteration', models.IntegerField()),
+                ('model_file_snapshot', models.FileField(storage=common.backends.DeconstructibleS3BotoStorage(), max_length=255, null=True, upload_to=cnntools.common_utils.FileUploadPath(b'cnntools', ext=b'prototxt'), blank=True)),
+                ('solver_file_snapshot', models.FileField(storage=common.backends.DeconstructibleS3BotoStorage(), max_length=255, null=True, upload_to=cnntools.common_utils.FileUploadPath(b'cnntools', ext=b'prototxt'), blank=True)),
+                ('deploy_file_snapshot', models.FileField(storage=common.backends.DeconstructibleS3BotoStorage(), max_length=255, null=True, upload_to=cnntools.common_utils.FileUploadPath(b'cnntools', ext=b'prototxt'), blank=True)),
                 ('outputs_json', models.TextField()),
                 ('output_names_json', models.TextField()),
-                ('net', models.ForeignKey(related_name='training_runs', to='cnntools.CaffeCNN')),
+                ('modified', models.DateTimeField(auto_now=True)),
+                ('net', models.ForeignKey(related_name='training_runs', on_delete=django.db.models.deletion.PROTECT, to='cnntools.CaffeCNN')),
             ],
             options={
                 'ordering': ['-id'],
                 'abstract': False,
             },
             bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='caffecnnsnapshot',
+            name='training_run',
+            field=models.ForeignKey(related_name='snapshots', on_delete=django.db.models.deletion.PROTECT, to='cnntools.CaffeCNNTrainingRun'),
+            preserve_default=True,
         ),
     ]
