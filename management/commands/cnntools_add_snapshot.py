298a06747d990d1f6594d96209b4310b5ac0b060
Balazs Kovacs <kovacs@adobe.com>
2016-06-13 19:01:49 +0000

    Improved cnntools_add_snapshot script parameter handling

--- management/commands/cnntools_add_snapshot.py
+++ management/commands/cnntools_add_snapshot.py
@@ -1,10 +1,7 @@
-import os
-
-from django.core.management.base import BaseCommand
-
 from cnntools.models import CaffeCNN, CaffeCNNTrainingRun
 from cnntools.snapshot_utils import upload_snapshot
 from cnntools.utils import create_default_trrun
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
@@ -13,26 +10,33 @@ class Command(BaseCommand):
             ' create a dummy trainingrun to accommodate the snapshot.')
 
     def handle(self, *args, **option):
-        if len(args) != 4:
+        if len(args) < 2:
             print 'Incorrect number of arguments!'
             print 'Usage: ./manage.py cnntools_add_snapshot [options] %s' % Command.args
             return
 
         netid = args[0]
-        trainingrun_id = int(args[1])
-        iteration = int(args[2])
-        weightfile_path = args[3]
+        weightfile_path = args[1]
+        if len(args) > 2:
+            trainingrun_id = int(args[2])
+        else:
+            trainingrun_id = None
+
+        if len(args) > 3:
+            iteration = int(args[3])
+        else:
+            iteration = 100000
 
         if not CaffeCNN.objects.filter(netid=netid).exists():
             raise ValueError(
                 'Couldn\'t find a CaffeCNN with the specified netid: {}'.format(netid)
             )
-        caffe_cnn = CaffeCNN.objects.get(netid=netid)
-        model_file_content = caffe_cnn.get_model_file_content()
-        solver_file_content = caffe_cnn.get_solver_file_content()
-        deploy_file_content = caffe_cnn.get_deploy_file_content()
-
         if not trainingrun_id:
+            caffe_cnn = CaffeCNN.objects.get(netid=netid)
+            model_file_content = caffe_cnn.get_model_file_content()
+            solver_file_content = caffe_cnn.get_solver_file_content()
+            deploy_file_content = caffe_cnn.get_deploy_file_content()
+
             # Create a dummy trainingrun for the snapshot
             trrun = create_default_trrun(
                 caffe_cnn,
@@ -54,3 +58,4 @@ class Command(BaseCommand):
                 )
 
         upload_snapshot(trainingrun_id, weightfile_path, iteration)
+
