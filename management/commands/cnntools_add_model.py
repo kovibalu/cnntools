c17c4218086ae310e0ab3024d99e08a38d11624a
Balazs Kovacs <kovacs@adobe.com>
2016-10-11 17:51:52 +0000

    Added html_short_description option

--- management/commands/cnntools_add_model.py
+++ management/commands/cnntools_add_model.py
@@ -1,16 +1,14 @@
-import os
-
 from cnntools.models import CaffeCNN
 from cnntools.utils import random_file_from_content
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    args = '<netid> <modelfile_relpath> <solverfile_relpath> <deployfile_relpath> <description>'
+    args = '<netid> <modelfile_relpath> <solverfile_relpath> <deployfile_relpath> <description> <html_short_description?>'
     help = 'Adds a CaffeCNN model to the database'
 
     def handle(self, *args, **option):
-        if len(args) != 5:
+        if len(args) < 5:
             print 'Incorrect number of arguments!'
             print 'Usage: ./manage.py cnntools_add_model [options] %s' % Command.args
             return
@@ -20,6 +18,10 @@ class Command(BaseCommand):
         solverfile_path = args[2]
         deployfile_path = args[3]
         description = args[4]
+        if len(args) >= 6:
+            html_short_description = args[5]
+        else:
+            html_short_description = ''
 
         # If the files are already there, delete them first
         if CaffeCNN.objects.filter(netid=netid).exists():
@@ -29,13 +31,15 @@ class Command(BaseCommand):
             caffe_cnn.deploy_file.delete()
             caffe_cnn.save()
 
-        CaffeCNN.objects.update_or_create(
-            netid=netid,
-            defaults={
-                'description': description,
-                'model_file': random_file_from_content(open(modelfile_path).read()),
-                'solver_file': random_file_from_content(open(solverfile_path).read()),
-                'deploy_file': random_file_from_content(open(deployfile_path).read()),
-            }
+        defaults = dict(
+            description=description,
+            model_file=random_file_from_content(open(modelfile_path).read()),
+            solver_file=random_file_from_content(open(solverfile_path).read()),
+            deploy_file=random_file_from_content(open(deployfile_path).read()),
         )
+        # Only update it is not empty
+        if html_short_description:
+            defaults['html_short_description'] = html_short_description
+
+        CaffeCNN.objects.update_or_create(netid=netid, defaults=defaults)
 
